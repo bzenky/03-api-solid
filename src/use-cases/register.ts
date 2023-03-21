71c4e9255770c88af5b1f9c0003fd58e8cf1574d
Zenky <bzenky@gmail.com>
2023-03-20 22:19:45 -0300

    feat: password hash test

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,4 +1,5 @@
 import { UsersRepository } from '@/repositories/users-repository'
+import { User } from '@prisma/client'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
@@ -6,6 +7,10 @@ interface RegisterUseCaseRequest {
   name: string
   email: string
   password: string
+}
+
+interface RegisterUseCaseResponse {
+  user: User
 }
 
 // SOLID
@@ -18,7 +23,11 @@ interface RegisterUseCaseRequest {
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) { }
 
-  async handle({ name, email, password }: RegisterUseCaseRequest) {
+  async handle({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6)
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
@@ -26,10 +35,14 @@ export class RegisterUseCase {
       throw new UserAlreadyExistsError()
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     })
+
+    return {
+      user,
+    }
   }
 }
