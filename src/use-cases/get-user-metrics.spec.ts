6fa1d3449f2f8d08bc8fdf9e2f426f08c53f6a2e
bzenky <bzenky@gmail.com>
2024-03-24 14:52:31 -0300

    feat: get user check-ins count use case

---
+++ src/use-cases/get-user-metrics.spec.ts
@@ -0,0 +1,31 @@
+import { expect, describe, it, beforeEach } from "vitest";
+import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
+import { GetUserMetricsUseCase } from "./get-user-metrics";
+
+let checkInsRepository: InMemoryCheckInsRepository;
+let sut: GetUserMetricsUseCase;
+
+describe("Fetch Check-in History Use Case", () => {
+  beforeEach(async () => {
+    checkInsRepository = new InMemoryCheckInsRepository();
+    sut = new GetUserMetricsUseCase(checkInsRepository);
+  });
+
+  it("should be able to get user check-ins count from metrics", async () => {
+    await checkInsRepository.create({
+      gym_id: "Xablau",
+      user_id: "user-01",
+    });
+
+    await checkInsRepository.create({
+      gym_id: "Xablau 02",
+      user_id: "user-01",
+    });
+
+    const { checkInsCount } = await sut.execute({
+      userId: "user-01",
+    });
+
+    expect(checkInsCount).toEqual(2);
+  });
+});
