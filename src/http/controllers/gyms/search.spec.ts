6d30dffc66bc8dba9bf0ceabcf38f588388ab831
bzenky <bzenky@gmail.com>
2024-04-02 22:03:24 -0300

    feat: search gym test:e2e

---
+++ src/http/controllers/gyms/search.spec.ts
@@ -0,0 +1,55 @@
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import request from "supertest";
+import { app } from "@/app";
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
+
+describe("Search Gym (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to create a gym", async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    await request(app.server)
+      .post("/gyms")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "Xablau 01 Gym",
+        description: "Test Gym",
+        phone: "99999999",
+        latitude: -27.228752,
+        longitude: -49.6401092,
+      });
+
+    await request(app.server)
+      .post("/gyms")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "Sunken Gym",
+        description: "Test Gym",
+        phone: "99999999",
+        latitude: -27.228752,
+        longitude: -49.6401092,
+      });
+
+    const response = await request(app.server)
+      .get("/gyms/search")
+      .set("Authorization", `Bearer ${token}`)
+      .query({
+        query: "Xablau",
+        page: 1,
+      })
+      .send();
+
+    expect(response.statusCode).toEqual(201);
+    expect(response.body.gyms).toHaveLength(1);
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({ title: "Xablau 01 Gym" }),
+    ]);
+  });
+});
