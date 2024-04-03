35f6dc8aa8a16b184dc4877006de275ceccd1ece
bzenky <bzenky@gmail.com>
2024-04-02 21:34:52 -0300

    feat: metrics controller

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -2,9 +2,12 @@ import { FastifyInstance } from "fastify";
 import { verifyJWT } from "@/http/middlewares/verify-jwt";
 import { create } from "./create";
 import { validate } from "./validate";
+import { metrics } from "./metrics";
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
+
+  app.get("/check-ins/metrics", metrics);
 
   app.post("/gyms/:gymId/check-ins", create);
   app.patch("/check-ins/:checkInId/validate", validate);
